ca6e028006b0d0262ec3557658193a7bf5cb87f2
Xuân Vinh <ngohoangxuanvinh1995@gmail.com>
2020-01-31 11:13:38 -0800

    update note controller

--- src/main/java/com/xv/admindashboard/controller/NoteController.java
+++ src/main/java/com/xv/admindashboard/controller/NoteController.java
@@ -7,7 +7,9 @@ import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @CrossOrigin(origins = "*")
@@ -26,8 +28,33 @@ public class NoteController {
     }
 
     @PreAuthorize("hasRole('ADMIN')")
+    @GetMapping("/notes/{id}")
+    public Note getNote(@PathVariable(name = "id") long noteId) throws Exception {
+        return this.noteRepository.findById(noteId).orElseThrow(() -> new Exception("Note not found " + noteId));
+    }
+
+    @PreAuthorize("hasRole('ADMIN')")
     @PostMapping("/notes")
     public Note createNote(@Valid @RequestBody Note note) {
         return this.noteRepository.save(note);
+    }
+
+    @PreAuthorize("hasRole('ADMIN')")
+    @PutMapping("notes/{id}")
+    public Note updateNote(@PathVariable(name = "id") long noteId, @Valid @RequestBody Note noteUpdate) throws Exception {
+        Note note = this.noteRepository.findById(noteId).orElseThrow(() -> new Exception("Note not found " + noteId));
+        note.setTitle(noteUpdate.getTitle());
+        note.setContent(noteUpdate.getContent());
+        return this.noteRepository.save(note);
+    }
+
+    @PreAuthorize("hasRole('ADMIN')")
+    @DeleteMapping("notes/{id}")
+    public Map<String, Boolean> deleteNote(@PathVariable(name = "id") long noteId) throws Exception {
+        Note note = this.noteRepository.findById(noteId).orElseThrow(() -> new Exception("Note not found " + noteId));
+        this.noteRepository.delete(note);
+        Map<String, Boolean> response = new HashMap<>();
+        response.put("deleted", Boolean.TRUE);
+        return response;
     }
 }
